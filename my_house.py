714c91d83303e8923c9ba2b7d9908dedf29943a2
nikitasmertyak <smertyak.n@phystech.edu>
2021-06-23 18:51:18 +0300

    Анимация домика
    Появилась функция tick() на таймере
    Сделал анимацию домика со стиранием всего на холсте.

--- my_house.py
+++ my_house.py
@@ -1,12 +1,17 @@
 from graph import *
 
+WIDTH = 150
+HEIGHT = 140
+
 def paint_house(x, y, width, height):
     foundation_height = int(height * 0.1)
     walls_height = int(height * 0.6)
     roof_height = height - foundation_height - walls_height
+    foundation_ledge = int(width * 0.05)
+    walls_width = width - foundation_ledge * 2
     #paint_foundation(x, y + height, int(width * 1, 1), height * 0.1)
     paint_foundation(x, y + walls_height, width, foundation_height)
-    paint_walls(x, y, width, walls_height)
+    paint_walls(x + foundation_ledge, y, walls_width, walls_height)
     paint_roof(x, y, width, roof_height)
 
 
@@ -26,13 +31,30 @@ def paint_roof(x, y, width, height):
     brushColor("yellow")
     penColor("black")
     polygon([(x, y), (x + width, y), ((x + width// 2) , (y - height))])
+
+
+def clear_canvas():
+    for item in canvas().find_all():
+        canvas().delete(item)
+
+
+
+def tick():
+    global x, y, scale
+    x = x + 2
+    y = y - 1
+    scale = scale - 0.3
+    clear_canvas()
+    paint_house(x, y, int(WIDTH * scale / 100), int(HEIGHT * scale / 100))
     
 
-
-
-
 def main():
-    paint_house(150, 150, 120, 100)
+    global x, y, scale
+    x = 150
+    y = 150
+    scale = 100
+    paint_house(x, y, int(WIDTH * scale / 100), int(HEIGHT * scale / 100))
+    onTimer(tick, 50)
     run()
 
 main()
