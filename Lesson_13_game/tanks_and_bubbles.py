9251111821119d886f1f0071e79e257582242255
nikitasmertyak <smertyak.n@phystech.edu>
2021-07-01 18:45:53 +0300

    Первая работающая версия игры
    Первые реализации классов: танка, снаряда, пузырика
    Игровой раунд создает танк и пузырики.

--- Lesson_13_game/tanks_and_bubbles.py
+++ Lesson_13_game/tanks_and_bubbles.py
@@ -6,6 +6,7 @@ DT =  0.2
 GRAVITY_CONST = 3
 START_PAUSE = 1000
 FRAME_TIME = 20
+TANK_RADIUS = 25
 
 
 # ========= Model ==============
@@ -14,14 +15,25 @@ class Tank:
     """ Танк, который будет порождать снаряды
         и тем самым сбивать пузыри.
     """
-    pass
-
+    def __init__(self, x, y, canvas):
+        self.r =  r = TANK_RADIUS
+        self.x = x
+        self.y = y
+        self._canvas = canvas
+        self.id = canvas.create_oval(x - r, y - r, x + r, y + r, fill="darkgreen")
 
 class Shell:
     """ Снаряд, который летит под действием гравитации,
         вылетая за границу экрана просто исчезает
     """
-    pass
+    def __init__(self, x, y, vx, vy, r, canvas):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.r = r
+        self._canvas = canvas
+        self.id = canvas.create_oval(x - r, y - r, x + r, y + r, fill="black")
 
 
 class Bubble:
@@ -30,7 +42,14 @@ class Bubble:
         отражается от стенок
         и идеале упруго отталкивается от других
     """
-    pass
+    def __init__(self, x, y, vx, vy, r, canvas):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.r = r
+        self._canvas = canvas
+        self.id = canvas.create_oval(x - r, y - r, x + r, y + r, fill="cyan")
 
 
 # ========== Control and View =============
@@ -43,8 +62,22 @@ class GameRound:
         3. список текущих, "живых" снарядов
         4. список пузыриков
     """
-    def __init__(self, canvas):
+    def __init__(self, canvas, difficulty = 1):
+        assert difficulty < 10
         self._canvas = canvas
+        self._tank = Tank(WIDTH // 2, HEIGHT, self._canvas)
+        self._shells = []
+        self.bubbles_number = difficulty * 5
+        self.bubbles_max_speed = difficulty * 2
+        self._targets = []
+        for i in range(self.bubbles_number):
+            r = randint(10 - difficulty, 25 - difficulty)
+            x = randint(r + 1, WIDTH - r - 1)
+            y = randint(r + 1, HEIGHT - r - 1)
+            Vx = randint(-self.bubbles_max_speed, +self.bubbles_max_speed)
+            Vy = randint(-self.bubbles_max_speed, +self.bubbles_max_speed)
+            bubble = Bubble(x, y, Vx, Vy, r, self._canvas)
+            self._targets.append(bubble)
 
 
     def handle_frame(self):
@@ -63,9 +96,10 @@ class MainWindow:
     """
     def __init__(self):
         self._root = tk.Tk()
-        self._restart_button = tk.Button(self._root, text="Перезапустить игру",
+        self._root.geometry(str(WIDTH + 10)+ 'x' + str(HEIGHT + 35))
+        self._restart_button = tk.Button(self._root, text="Запустить игру",
                                          command=self._restart_button_handler)
-        self._restart_button.pack()        
+        self._restart_button.pack()
         self._game = None
 
 
