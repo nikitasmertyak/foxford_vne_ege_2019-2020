0eab3166fd43e66b4da8458c773b3f13dd05fdc0
nikitasmertyak <smertyak.n@phystech.edu>
2021-07-18 19:45:59 +0300

    Добавил искуственный интелект

--- Lesson_20_theory_of_games/rocks_game.py
+++ Lesson_20_theory_of_games/rocks_game.py
@@ -3,12 +3,6 @@ GAME_OVER_ROCKS_NUMBER = 16
 def game_over(s: int):
     return s >= GAME_OVER_ROCKS_NUMBER
 
-def get_player_action():
-    x = int(input("Выберите ход (от 1 до 3):"))
-    while x not in {1, 2, 3}:
-        x = int(input("Так нельзя! Выберите ход (от 1 до 3):"))
-    return x
-
 def apply(s, action):
     if action == 1:
         return s + 1
@@ -19,7 +13,46 @@ def apply(s, action):
     else:
         raise AssertionError('Такого не должно быть никогда!')
 
-def print_game_state(s):
+def value_position(s):
+    """ Оценивает позицию s с точки зрения текущего игрока.
+        Возвращает:
+            -1, если позиция проигрышная
+            +1, если выигрышная,
+            0, если ничейная (в лучшем случае)
+    """
+    if s >= 16:
+        return -1  # это проигрышные позиции
+    min_value = 2  # начальное состояние для поиска минимума
+    for action in 1, 2, 3:
+        s_after_action = apply(s, action)
+        value_of_position_after_action = value_position(s_after_action)
+        if value_of_position_after_action < min_value:
+            min_value = value_of_position_after_action
+    # моя позиция настолько хороша, насколько плохую я нашёл для противника
+    return (-min_value)
+
+def get_ai_action(s):
+    best_action = None
+    min_value = 2  # "плохое", т.е. начальное состояние для поиска минимума
+    for action in 1, 2, 3:
+        s_after_action = apply(s, action)
+        value_of_position_after_action = value_position(s_after_action)
+        if value_of_position_after_action < min_value:
+            min_value = value_of_position_after_action
+            best_action = action
+    print("Искусственный интеллект решил",
+          "прибавить 1 камень." if best_action == 1 else \
+          "прибавить 3 камня." if best_action == 2 else \
+          "увеличить к-во камней вдвое!")
+    return best_action
+
+def get_player_action(s: int):
+    x = int(input("Выберите ход (от 1 до 3):"))
+    while x not in {1, 2, 3}:
+        x = int(input("Так нельзя! Выберите ход (от 1 до 3):"))
+    return x
+
+def print_game_state(s: int):
     print('Теперь в куче', s, 'камней.')
 
 def game_round(s: int):
@@ -33,17 +66,17 @@ def game_round(s: int):
     current_player = 1
     
     while not game_over(s):
-        action = get_player_action()
+        action = get_player_action(s)
         s = apply(s, action)
         current_player = 2
         print_game_state(s)
         if game_over(s):
             break
-        action = get_player_action()
+        action = get_ai_action(s)
         s = apply(s, action)
         current_player = 1
         print_game_state(s)
     
     print('Победил игрок', 3 - current_player)
 
-game_round(7)
+game_round(1)
